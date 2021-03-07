238d9063875fe72d05c798e6ca526d0ec1055867
Ari Porad <ari@ariporad.com>
2021-03-07 18:20:32 -0500

    feat: add solution completess checking

--- hw1/main.py
+++ hw1/main.py
@@ -1,5 +1,65 @@
 from itertools import islice
 
+class Solution:
+	"""
+	Class representing a (possibly-incomplete) solution to the problem.
+
+	This class is heavily context-dependent, and the user is expected to know what's going on with
+	it. To that end, it doesn't store things like order (number of sides to each nut), completeness,
+	etc. This is for simplicity.
+	"""
+
+	def __init__(self, center_nut, nuts_to_place, placed_nuts = []):
+		# The center nut and nuts_to_place are specified as in the canonical format (see
+		# `canonicalize_nut`). 
+		self.center_nut = center_nut
+		self.nuts_to_place = nuts_to_place
+
+		# Each index `i` is the nut that's placed on the edge `i` of the center nut (ie. aligned
+		# with the number at `self.center_nut[i]`). These nuts are rotated such that their first
+		# element is the one touching the center nut.
+		self.placed_nuts = placed_nuts
+
+	def is_invalid(self):
+		"""
+		Validate that the solution doesn't break any rules. **DOES NOT** ensure that the solution is
+		complete, so as to enable validating partial solutions. (ie. you should call both is_valid
+		and is_complete to ensure the puzzle is solved.)
+
+		Returns a string explaining (one of the reason(s)) why the solution is invalid, or False if
+		the solution is valid.
+		"""
+		for i, nut in enumerate(self.placed_nuts):
+			# First, check the center connection
+			if self.center_nut[i] != nut[0]:
+				return f"Center[{i}] = {self.center_nut[i]} != nut[{i}][0] = {nut[0]}"
+
+			# Then, check the nut's neighbor. We only check one neighbor per nut (the one with the
+			# next index/clockwise), because if all the nuts are in place this will result in all
+			# the connections being checked exactly once.
+
+			next_nut_i = (i + 1) % len(self.placed_nuts)
+			if not self.placed_nuts[next_nut_i]: continue # If the next nut hasn't been placed
+			if self.placed_nuts[next_nut_i][1] != nut[-1]:
+				return f"Edge: nut[{i + 1}][0] = {self.placed_nuts[next_nut_i][0]} != nut[{i}][-1] = {nut[-1]}"
+
+		return False
+
+	def is_complete(self):
+		"""
+		Validate that the solution is complete (ie. all nuts are placed). **DOES NOT** ensure that
+		no rules have been broken. (ie. you should call both is_valid and is_complete to ensure the
+		puzzle is solved.)
+
+		>>> solution = Solution((1, 2, 3), [], [(1, 2, 3), (2, 3, 1), (3, 1, 2)])
+		>>> solution.is_complete()
+		True
+		>>> solution.is_invalid()
+		False
+		"""
+		return len(self.nuts_to_place) == 0 and len(self.placed_nuts) == len(self.center_nut)
+
+
 def maplist(fn, items):
 	"""Exactly like map, but returns a list."""
 
@@ -79,7 +139,7 @@ def unique(items):
 
 def canonicalize_nut(nut):
 	"""
-	Rotate a nut such that 1 is first.
+	Rotate a nut such that 1 is first. The elements in the list are in clockwise order.
 
 	>>> canonicalize_nut((4, 5, 6, 1, 2, 3))
 	(1, 2, 3, 4, 5, 6)
@@ -93,7 +153,7 @@ def generate_puzzle(n):
 	Generate a nuts-style puzzle of n+1 n-sided regular polygons with a number 1 through n on each
 	side.
 	"""
-	
+
 	numbers = range(1, n + 1)
 	all_nuts = permute(numbers)
 	all_nuts_canonicalized = map(canonicalize_nut, all_nuts)
