a71c188ff0d106020245cc073374aa98bf7eb4c6
Ari Porad <ari@ariporad.com>
2021-03-07 16:44:30 -0500

    feat: properly generate puzzles
    Only works in reasonable time for n <= 9

--- hw1/main.py
+++ hw1/main.py
@@ -7,21 +7,60 @@ def permute(items):
 	Return all possible permutations of items.
 
 	>>> permute([1, 2, 3])
-	[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+	[(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
 	"""
 
 	if len(items) == 0: return items
-	if len(items) == 1: return [items]
+	if len(items) == 1: return [tuple(items)]
 
 	ret = []
 
 	for x in items:
 		ret = ret + [
-			[x] + permuted for permuted in permute([y for y in items if y != x])
+			tuple((x,) + permuted) for permuted in permute([y for y in items if y != x])
 		]
 
 	return ret
 
+def rotate(items, num):
+	"""
+	Rotate a list or tuple.
+
+	>>> rotate([1, 2, 3, 4, 5], 3)
+	[4, 5, 1, 2, 3]
+	>>> rotate((1, 2, 3, 4, 5), 3)
+	(4, 5, 1, 2, 3)
+	"""
+	return items[num:] + items[:num]
+
+def unique(items):
+	"""
+	Deduplicate a list. No guarantees are made about order.
+
+	>>> unique([1, 2, 3, 3, 1])
+	[1, 2, 3]
+	"""
+	# HACK: This is a fast and easy implementation, even if it's a little ugly
+	return list(set(items))
+
+def canonicalize_nut(nut):
+	"""
+	Rotate a nut such that 1 is first.
+
+	>>> canonicalize_nut((4, 5, 6, 1, 2, 3))
+	(1, 2, 3, 4, 5, 6)
+	"""
+	return rotate(nut, nut.index(1))
+
+
+def generate_puzzle(n):
+	"""
+	Generate a nuts-style puzzle of n+1 n-sided regular polygons with a number 1 through n on each
+	side.
+	"""
+	return unique(map(canonicalize_nut, permute(range(1, n + 1))))[0:n+1]
 
 if __name__ == "__main__":
-	permute([1, 2, 3])
+	import doctest
+	doctest.testmod()
+	print(generate_puzzle(6))
